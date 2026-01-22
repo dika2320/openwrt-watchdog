82f32d6e07100775ddd321566b43a95f77cbf895
dika <150585392+dika2320@users.noreply.github.com>
2026-01-22 21:18:59 +0700

    Update interface_watchdog.sh

--- scripts/interface_watchdog.sh
+++ scripts/interface_watchdog.sh
@@ -4,30 +4,27 @@ PING_TARGET1="8.8.8.8"
 PING_TARGET2="1.1.1.1"
 WAIT_AFTER_RESTART=30
 
-
-INTERFACE=$(ubus call network.interface.wan status 2>/dev/null | \
+# Otomatis deteksi L3 Device (misal eth1)
+L3_DEV=$(ubus call network.interface.wan status 2>/dev/null | \
             grep -o '"l3_device": *"[^"]*"' | cut -d'"' -f4)
 
+[ -z "$L3_DEV" ] && L3_DEV=$(ip route | awk '/default/ {print $5; exit}')
+[ -z "$L3_DEV" ] && L3_DEV="eth1"
 
-[ -z "$INTERFACE" ] && INTERFACE=$(ip route | awk '/default/ {print $5; exit}')
+# Menentukan Logical Interface (Nama di LuCI)
+# Biasanya modem NCM ada di interface bernama 'wan'
+LOGICAL_IF="wan" 
 
+logger -t IFRESTART "Cek koneksi internet via $L3_DEV ($LOGICAL_IF)"
 
-[ -z "$INTERFACE" ] && INTERFACE="eth1"
-
-logger -t IFRESTART "Cek koneksi internet via $INTERFACE"
-
-ping -c 3 -W 3 $PING_TARGET1 >/dev/null 2>&1 || \
-ping -c 3 -W 3 $PING_TARGET2 >/dev/null 2>&1
-
-if [ $? -ne 0 ]; then
-    logger -t IFRESTART "Internet DOWN, restart interface $INTERFACE"
-
-
-    ifdown wan
-    sleep 5
-    ifup wan
-
-    sleep $WAIT_AFTER_RESTART
-else
+if ping -c 3 -W 3 $PING_TARGET1 >/dev/null 2>&1 || \
+   ping -c 3 -W 3 $PING_TARGET2 >/dev/null 2>&1; then
     logger -t IFRESTART "Internet OK"
+    exit 0
+else
+    logger -t IFRESTART "Internet DOWN, restart interface $LOGICAL_IF"
+    ifdown $LOGICAL_IF
+    sleep 5
+    ifup $LOGICAL_IF
+    sleep $WAIT_AFTER_RESTART
 fi
